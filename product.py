1b07dffb4fb0385eb9c7273874badf3024461d64
island <scarcehere88@gmail.com>
2022-10-14 14:04:29 -0400

    Units produced method

--- product.py
+++ product.py
@@ -1,3 +1,5 @@
+import random
+
 class Product:
     """"The class containing information about the product, and associated mathods."""
     def __init__(self, ProductCode, ProductName, SalePrice, ManufactureCost, StockLevel, EstMonthlyUnits):
@@ -22,3 +24,24 @@ class Product:
         return self._StockLevel
     def EstMonthlyUnits(self):
         return self._EstMonthlyUnits
+
+    #Monthly units produced
+
+    def ActMonthlyUnits(self):
+        MonthlyUnits = []
+        MonthlySales = []
+        StockRecord = []
+        for i in range(12):
+            MonthlyUnits.append(self._EstMonthlyUnits + random.randint(-10, 10))
+            MonthlySales.append(self._EstMonthlyUnits + random.randint(-10, 10))
+            if i == 0:
+                StockRecord.append(self._StockLevel + MonthlyUnits[0] - MonthlySales[0])
+            else:
+                StockRecord.append(StockRecord[i - 1] + MonthlyUnits(i) - MonthlySales(i))
+
+        self._MonthlyStock = StockRecord
+        self._MonthlyUnits = MonthlyUnits
+        self._MonthlySales = MonthlySales
+        return StockRecord
+
+    def 
